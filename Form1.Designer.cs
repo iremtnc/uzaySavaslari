2fdf7cbeeafca43979361954a1aad2685622ccff
itsme <itsme@DESKTOP-PTSOEIK>
2024-06-03 12:45:50 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace uzaySavaslari
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBoxGalaksi = new System.Windows.Forms.PictureBox();
+            this.pictureBoxOyuncuGemi = new System.Windows.Forms.PictureBox();
+            this.labelPuan = new System.Windows.Forms.Label();
+            this.timerOyuncu = new System.Windows.Forms.Timer(this.components);
+            this.timerMermiFirlat = new System.Windows.Forms.Timer(this.components);
+            this.timerDusmanVur = new System.Windows.Forms.Timer(this.components);
+            this.timerDusmanOlustur = new System.Windows.Forms.Timer(this.components);
+            this.timerMermiKontrol = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGalaksi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOyuncuGemi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxGalaksi
+            // 
+            this.pictureBoxGalaksi.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxGalaksi.Image = global::uzaySavaslari.Properties.Resources.Evren_04;
+            this.pictureBoxGalaksi.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxGalaksi.Name = "pictureBoxGalaksi";
+            this.pictureBoxGalaksi.Size = new System.Drawing.Size(461, 591);
+            this.pictureBoxGalaksi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxGalaksi.TabIndex = 0;
+            this.pictureBoxGalaksi.TabStop = false;
+            // 
+            // pictureBoxOyuncuGemi
+            // 
+            this.pictureBoxOyuncuGemi.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxOyuncuGemi.Image = global::uzaySavaslari.Properties.Resources.Gemi_01;
+            this.pictureBoxOyuncuGemi.Location = new System.Drawing.Point(177, 459);
+            this.pictureBoxOyuncuGemi.Name = "pictureBoxOyuncuGemi";
+            this.pictureBoxOyuncuGemi.Size = new System.Drawing.Size(87, 68);
+            this.pictureBoxOyuncuGemi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxOyuncuGemi.TabIndex = 1;
+            this.pictureBoxOyuncuGemi.TabStop = false;
+            this.pictureBoxOyuncuGemi.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // labelPuan
+            // 
+            this.labelPuan.AutoSize = true;
+            this.labelPuan.BackColor = System.Drawing.Color.Transparent;
+            this.labelPuan.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelPuan.ForeColor = System.Drawing.Color.White;
+            this.labelPuan.Location = new System.Drawing.Point(12, 18);
+            this.labelPuan.Name = "labelPuan";
+            this.labelPuan.Size = new System.Drawing.Size(101, 29);
+            this.labelPuan.TabIndex = 2;
+            this.labelPuan.Text = "Puan=0 ";
+            // 
+            // timerOyuncu
+            // 
+            this.timerOyuncu.Interval = 1;
+            this.timerOyuncu.Tick += new System.EventHandler(this.timerOyuncu_Tick);
+            // 
+            // timerMermiFirlat
+            // 
+            this.timerMermiFirlat.Interval = 10;
+            this.timerMermiFirlat.Tick += new System.EventHandler(this.timerMermiFirlat_Tick);
+            // 
+            // timerDusmanVur
+            // 
+            this.timerDusmanVur.Interval = 10;
+            this.timerDusmanVur.Tick += new System.EventHandler(this.timerDusmanVur_Tick);
+            // 
+            // timerDusmanOlustur
+            // 
+            this.timerDusmanOlustur.Interval = 2000;
+            this.timerDusmanOlustur.Tick += new System.EventHandler(this.timerDusmanOlustur_Tick);
+            // 
+            // timerMermiKontrol
+            // 
+            this.timerMermiKontrol.Tick += new System.EventHandler(this.timerMermiKontrol_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 591);
+            this.Controls.Add(this.labelPuan);
+            this.Controls.Add(this.pictureBoxOyuncuGemi);
+            this.Controls.Add(this.pictureBoxGalaksi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGalaksi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOyuncuGemi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxGalaksi;
+        private System.Windows.Forms.PictureBox pictureBoxOyuncuGemi;
+        private System.Windows.Forms.Label labelPuan;
+        private System.Windows.Forms.Timer timerOyuncu;
+        private System.Windows.Forms.Timer timerMermiFirlat;
+        private System.Windows.Forms.Timer timerDusmanVur;
+        private System.Windows.Forms.Timer timerDusmanOlustur;
+        private System.Windows.Forms.Timer timerMermiKontrol;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uzaySavaslari
+{
+    public partial class Form1 : Form
+    {
+        int formYukseklik = 700, formGenislik = 500;
+        //Oyuncu gemisinin koordinatları(duracağı yer) 
+        int Oyuncu_X = 200, Oyuncu_Y = 400;
+        int OyuncuHiz_yatay = 0, OyuncuHizDusey = 0;
+        int puan;
+        List<PictureBox> mermiler = new List<PictureBox>();
+        int MermiHiz = 10;
+        List<PictureBox> Dusmanlar = new List<PictureBox>();
+        int DusmanHiz = 2; 
+
+        Random rnd= new Random();   
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timerOyuncu_Tick(object sender, EventArgs e)
+        {
+            Oyuncu_X += OyuncuHiz_yatay;
+            Oyuncu_Y += OyuncuHizDusey;
+            // Form sınırlarını kontrol edin
+            if (Oyuncu_X < 0)
+                Oyuncu_X = 0;
+            else if (Oyuncu_X + pictureBoxOyuncuGemi.Width > formGenislik)
+                Oyuncu_X = formGenislik - pictureBoxOyuncuGemi.Width;
+
+            if (Oyuncu_Y < 0)
+                Oyuncu_Y = 0;
+            else if (Oyuncu_Y + pictureBoxOyuncuGemi.Height > formYukseklik)
+                Oyuncu_Y = formYukseklik - pictureBoxOyuncuGemi.Height;
+
+            // Yeni konumu pictureBox'a atayın
+            pictureBoxOyuncuGemi.Location = new Point(Oyuncu_X, Oyuncu_Y); 
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Height = formYukseklik;
+            this.Width = formGenislik;
+
+            //Objelerimizi galaksinin içine alıyoruz 
+            
+            pictureBoxOyuncuGemi.Parent = pictureBoxGalaksi; 
+            labelPuan.Parent=pictureBoxGalaksi;
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int hiz = 5;
+             switch (e.KeyCode)
+            {
+                case Keys.Left: OyuncuHiz_yatay -= hiz; break;
+                case Keys.Right: OyuncuHiz_yatay += hiz; break;
+                case Keys.Up: OyuncuHizDusey -= hiz; break;
+                case Keys.Down: OyuncuHizDusey += hiz; break;
+                case Keys.Enter:
+                    puan = 0;
+                    timerOyuncu.Start();
+                    timerMermiFirlat.Start();
+                    timerDusmanOlustur.Start();
+                    timerDusmanVur.Start();
+                    timerMermiKontrol.Start();  
+                    break;
+                case Keys.Space:
+                    MermiOlustur();
+                    break; 
+            }
+        }
+
+        private void timerMermiFirlat_Tick(object sender, EventArgs e)
+        {
+            MermileriFırlat();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left: OyuncuHiz_yatay = 0; break;
+                case Keys.Right: OyuncuHiz_yatay = 0; break;
+                case Keys.Up: OyuncuHizDusey = 0; break;
+                case Keys.Down: OyuncuHizDusey = 0; break;
+            }
+        }
+        public void MermiOlustur()
+        {
+            PictureBox Mermi = new PictureBox
+            {
+                Size = new Size(15, 15),
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Image=Properties.Resources.Laser_08,
+                BackColor=Color.Transparent
+            };
+            int mermilocX = pictureBoxOyuncuGemi.Location.X + pictureBoxOyuncuGemi.Width / 2 - 5;
+            int mermilocY = pictureBoxOyuncuGemi.Top - 1;
+
+            Mermi.Location=new Point(mermilocX, mermilocY);
+            Controls.Add(Mermi);
+            Mermi.Parent=pictureBoxGalaksi;
+            Mermi.BringToFront();  //galaxy resminin üstüne çıkartıyoruz 
+            mermiler.Add(Mermi);
+        }
+
+        private void timerDusmanVur_Tick(object sender, EventArgs e)
+        {
+           DusmanVur();
+        }
+
+        private void timerDusmanOlustur_Tick(object sender, EventArgs e)
+        {
+            DusmanOlustur();
+        }
+
+        private void timerMermiKontrol_Tick(object sender, EventArgs e)
+        {
+            for (int m=0; m<mermiler.Count; m++)
+            {
+                for (int d = 0; d < Dusmanlar.Count; d++)
+                {
+                    try
+                    {
+                        //Mermi ile düşman gemisi çarpıştı mı? 
+                        if (mermiler[m].Bounds.IntersectsWith(Dusmanlar[d].Bounds))
+                        {
+                            puan += 1;
+                            labelPuan.Text = "Puan=" + puan.ToString();
+
+                            pictureBoxGalaksi.Controls.Remove(mermiler[m]);
+                            mermiler.Remove(mermiler[m]);
+
+                            pictureBoxGalaksi.Controls.Remove(Dusmanlar[d]);
+                            Dusmanlar.Remove(Dusmanlar[d]);
+
+                            //RAM hafıza temizleme 
+
+                            GC.Collect(); //çöp toplayıcı 
+                            GC.WaitForPendingFinalizers(); //çöpleri yok et 
+                        }
+                    } 
+                    //picturebox listesinden eleman silindiği için 
+                    //aynı indis tekrar kontrol edildiği zaman 
+                    //ilk aşamada null değer olduğu için 
+                    //return ile görmezden geliyoruz 
+
+                    catch (ArgumentOutOfRangeException)
+                    {
+                        return;
+                    }
+
+                }
+            }
+        }
+
+        public void MermileriFırlat()
+        {
+            for(int i=0; i<mermiler.Count; i++)
+            {
+                mermiler[i].Top -= MermiHiz; 
+            }
+        }
+
+        public void DusmanOlustur()
+        {
+            int yer = rnd.Next(0, formGenislik - 50);
+            PictureBox Dusman = new PictureBox
+            {
+                Size = new Size(50, 50),
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Image = Properties.Resources.Gemi_03,
+                BackColor = Color.Transparent,
+                Location = new Point(yer, 0),
+                Visible = true
+            };
+            Controls.Add(Dusman);  //forma ekledik 
+            Dusman.Parent= pictureBoxGalaksi;   //galaksi içine aldık 
+            Dusman.BringToFront(); //öne getirdik 
+            Dusmanlar.Add(Dusman); 
+        }
+
+        public void DusmanVur()
+        {
+            for(int i=0; i<Dusmanlar.Count; i++)
+            {
+                Dusmanlar[i].Top += DusmanHiz;
+                //Düşman gemisi formun dışına çıkarsa veya oyuncunun gemisine çarparsa 
+
+                if (Dusmanlar[i].Top>=formYukseklik-10 ||
+                    Dusmanlar[i].Bounds.IntersectsWith(pictureBoxOyuncuGemi.Bounds))  
+                {
+                    //Oyun bitti 
+                    OyunBitir();
+                }
+               
+ 
+            }
+        } 
+
+        public void OyunBitir()
+        {
+            timerOyuncu.Stop();
+            timerMermiFirlat.Stop();
+            timerMermiKontrol.Stop();
+            timerDusmanOlustur.Stop();
+            timerDusmanVur.Stop();
+
+            MessageBox.Show("Oyun bitti!");
+            
+        }
+    }
+}
+
